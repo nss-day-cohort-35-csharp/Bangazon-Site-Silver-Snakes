319f00d603f7a8c5c31b88eaf0fd824f734006a3
Heidi <hspradlin8@gmail.com>
2020-01-26 17:20:16 -0600

    product category requires a selection

--- Bangazon/Models/Product.cs
+++ Bangazon/Models/Product.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,19 +10,19 @@ namespace Bangazon.Models
     public class Product
     {
         [Key]
-        public int ProductId {get;set;}
+        public int ProductId { get; set; }
 
         [Required]
         [DataType(DataType.Date)]
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
-        public DateTime DateCreated {get;set;}
+        public DateTime DateCreated { get; set; }
 
         [Required]
         [StringLength(255)]
         public string Description { get; set; }
 
         [Required]
-        [StringLength(55, ErrorMessage="Please shorten the product title to 55 characters")]
+        [StringLength(55, ErrorMessage = "Please shorten the product title to 55 characters")]
         public string Title { get; set; }
 
         [Required]
@@ -32,19 +33,19 @@ namespace Bangazon.Models
         public int Quantity { get; set; }
 
         [Required]
-        public string UserId {get; set;}
+        public string UserId { get; set; }
 
-        public string City {get; set;}
+        public string City { get; set; }
 
-        public string ImagePath {get; set;}
+        public string ImagePath { get; set; }
 
         public bool Active { get; set; }
 
         [Required]
         public ApplicationUser User { get; set; }
 
-        [Required]
-        [Display(Name="Product Category")]
+        [Required(ErrorMessage = "Error: Must choose a product category")]
+        [Display(Name = "Product Category")]
         public int ProductTypeId { get; set; }
 
         public ProductType ProductType { get; set; }
@@ -52,9 +53,10 @@ namespace Bangazon.Models
         public virtual ICollection<OrderProduct> OrderProducts { get; set; }
 
 
-        public Product ()
+        public Product()
         {
             Active = true;
+
         }
 
     }
